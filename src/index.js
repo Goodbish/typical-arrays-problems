0485c660393f6ab6470efa714579e2f7facfabd4
Goodbish <43109002+Goodbish@users.noreply.github.com>
2020-09-29 14:27:21 +0500

    feat: 'added functions'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array == []) return 0;
+  array = array.sort(function(a, b) { return a - b; });
+  return array[0] || 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined) return 0;
+  array = array.sort(function(a, b) { return a - b; });
+  return array[array.length-1] || 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined) return 0;
+  array = array.sort(function(a, b) { return a - b; });
+  let average = array.reduce((sum, current) => sum + current, 0)/array.length;;
+  return average || 0;
 }
+
